9ca2b885aba2f5e2d96a66684ea10a0b2def1ff6
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2022-11-15 16:14:54 -0400

    Create basico2.py

---
+++ basico2.py
@@ -0,0 +1,11 @@
+#Exercicio: descobrir o IMC de Carlos Andrade
+#nome = 'Carlos Andrade'
+#altura = 1.80
+#peso = 80
+
+nome = 'Carlos Andrade'
+altura = 1.80
+peso = 80
+imc = peso / altura ** 2
+
+print (nome , "tem" , altura , "metros," , "pesa" , peso , "kilos e seu IMC vale" , imc) 
