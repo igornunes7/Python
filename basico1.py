297d51a32b39719b3a73d15e78bea8a915405147
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2022-11-15 15:51:02 -0400

    Create basico1.py

---
+++ basico1.py
@@ -0,0 +1,26 @@
+#Completar corretamente com as informações dentro dos print
+
+nome = 'Igor'
+sobrenome = 'Nunes'
+idade = 19
+ano_nascimento = 2002
+maior_de_idade = idade >= 18
+altura_metros = 1.79
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
+
+#Extra
+
+#Tipos das variavéis
+print (type(nome))
+print (type(sobrenome))
+print (type(idade))
+print (type(ano_nascimento))
+print (type(maior_de_idade))
+print (type(altura_metros))
