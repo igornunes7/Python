eb47cf0f7c02080db585788cf14cd3a9fb7c30f4
Igor Nunes <101185370+igornunes7@users.noreply.github.com>
2022-11-19 10:26:51 -0400

    Create basico9.py

---
+++ basico9.py
@@ -0,0 +1,26 @@
+"""
+Faça um programa que gere um número aleátorio entre 0 e 50 e o usuário deve acertá-lo
+"""
+
+from random import randint
+from time import sleep
+
+maquina = randint(0 , 50)
+print ("GERANDO UM NÚMERO ENTRE 0 E 50...")
+print ("-"*40)
+sleep (2.5)
+print ("TENTE ADVINHAR NO NUMERO QUE PENSEI")
+
+while True:
+    numero = int(input("Digite o número que você deseja: "))
+    if numero < 0 or numero > 50:
+        print ("Numéro inválido. Tente novamente!")
+    else:
+        if numero == maquina:
+            print ("Você acertou o número!")
+            break
+        else:
+            if numero < maquina:
+                print ("Quasee! Tente um número um pouco mais pra cima")
+            else:
+                print ("Quasee! Tente um número um pouco mais pra baixo")
